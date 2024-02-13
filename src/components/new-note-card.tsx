28eae37366a9d154c55fea03d42e5c7708173983
4llves <jhonatab4@gmail.com>
2024-02-13 18:03:25 -0300

    add modals

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,14 +1,43 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'lucide-react'
+
 export function NewNoteCard() {
   return (
-    <div className='rounded-md bg-slate-700 p-5 space-y-3'>          
-    <span className='text-sm font-medium text-slate-200'>
-      Adicionar Nota
-    </span>
+    <Dialog.Root>
+      <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-700 p-5 outline-none gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+      <span className='text-sm font-medium text-slate-200'>
+        Adicionar Nota
+      </span>
 
-    <p className='text-sm leading-6 text-slate-400'>
-      Grave uma nota em áudio que será convertida para texto automaticamente.
-    </p>
+      <p className='text-sm leading-6 text-slate-400'>
+        Grave uma nota em áudio que será convertida para texto automaticamente.
+      </p>
+      </Dialog.Trigger>
 
-    </div>
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/50'/>
+        <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+          <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 rounded-bl-lg hover:text-slate-100'>
+            <X className='size-5'/>
+          </Dialog.Close>
+          <div className="flex flex-1 flex-col gap-3 p-5">
+          <span className='text-sm font-medium text-slate-300'>
+            
+          </span>
+
+          <p className='text-sm leading-6 text-slate-400'>
+            Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.
+          </p>
+          </div>
+
+          <button 
+            type="button"
+            className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
+          >
+            Salvar nota
+          </button>
+        </Dialog.Content>        
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
